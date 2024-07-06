1e1ae71cffff0394d53f5d88a297bfa6ff08c477
mehmetyigit27 <159683775+mehmetyigit27@users.noreply.github.com>
2024-07-06 18:12:34 +0300

    Update phishingtool.sh

--- phishingtool.sh
+++ phishingtool.sh
@@ -17,11 +17,11 @@ printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;94m DURSHIKAX\e[0m      
 menu () {
 read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Which tool do you want to use? \e[0m\en' tool
 if [[ $tool == 1 ]]; then
-cd tools/durshikax
+cd tools/phishingtool
 bash durshikax.sh
 
 elif [[ $tool == 2 ]]; then
-cd tools/senior-mask
+cd tools/phishingtool1
 ./senior-mask.sh
 
 else
